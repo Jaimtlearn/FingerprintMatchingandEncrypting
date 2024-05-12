63f3837cf2c84b37bb94181954f4d1ac3d7b0c6d
Jaimit Patel <124894131+Jaimtlearn@users.noreply.github.com>
2024-05-12 10:51:02 +0530

    Update Encrypt.py

--- Encrypt.py
+++ Encrypt.py
@@ -1,57 +1,50 @@
-
+from dotenv import load_dotenv
 from Crypto import Random
 from Crypto.Cipher import AES
 import os
-import os.path
-from os import listdir
-from os.path import isfile, join
 import time
+import getpass
+load_dotenv()
 
-
-class Encryptor:
-    def __init__(self, key):
+class Encryption:
+    # Initializing Key
+    def __init__(self,key):
         self.key = key
 
-    def pad(self, s):
-        return s + b"\0" * (AES.block_size - len(s) % AES.block_size)
-
-    def encrypt(self, message, key, key_size=256):
-        message = self.pad(message)
-        iv = Random.new().read(AES.block_size)
-        cipher = AES.new(key, AES.MODE_CBC, iv)
-        return iv + cipher.encrypt(message)
-
-    def encrypt_file(self, file_name):
-        with open(file_name, 'rb') as fo:
-            plaintext = fo.read()
-        enc = self.encrypt(plaintext, self.key)
-        with open(file_name + ".enc", 'wb') as fo:
-            fo.write(enc)
-        os.remove(file_name)
-
-    def decrypt(self, ciphertext, key):
-        iv = ciphertext[:AES.block_size]
-        cipher = AES.new(key, AES.MODE_CBC, iv)
-        plaintext = cipher.decrypt(ciphertext[AES.block_size:])
+    # Adds padding to the Image
+    def padding(self,txt):
+        return txt + b"\0" * (AES.block_size - len(txt) % AES.block_size)
+    
+    # Encrypt Function
+    def encrypt(self,message,key,key_size=256):
+        message = self.padding(message)
+        IV = Random.new().read(AES.block_size)
+        Cipher_text = AES.new(key,AES.MODE_CBC, IV)
+        return IV + Cipher_text.encrypt(message)
+    
+    def encrypt_file(self,filename):
+        with open(filename,'rb') as f:
+            text = f.read()
+        enc = self.encrypt(text,self.key)
+        with open(filename + ".enc",'wb') as f:
+            f.write(enc)
+        os.remove(filename)
+    # Decrypt Function
+    def decrypt(self,text,key):
+        IV = text[:AES.block_size]
+        cipher = AES.new(key,AES.MODE_CBC,IV)
+        plaintext = cipher.decrypt(text[AES.block_size:])
         return plaintext.rstrip(b"\0")
 
-    def decrypt_file(self, file_name):
-        with open(file_name, 'rb') as fo:
-            ciphertext = fo.read()
-        dec = self.decrypt(ciphertext, self.key)
-        with open(file_name[:-4], 'wb') as fo:
-            fo.write(dec)
-        os.remove(file_name)
+    def decrypt_file(self,filename):
+        with open(filename,'rb') as f:
+            text = f.read()
+        dec = self.decrypt(text,self.key)
+        with open(filename[:-4],'wb') as f:
+            f.write(dec)
+        os.remove(filename)
 
-    def getAllFiles(self):
-        dir_path = os.path.dirname(os.path.realpath(__file__))
-        dirs = []
-        for dirName, subdirList, fileList in os.walk(dir_path):
-            for fname in fileList:
-                if (fname != 'script.py' and fname != 'data.txt.enc'):
-                    dirs.append(dirName + "\\" + fname)
-        return dirs
-
+    #get all files code
     def encrypt_all_files(self):
         dirs = self.getAllFiles()
         for file_name in dirs:
@@ -61,53 +54,51 @@ class Encryptor:
         dirs = self.getAllFiles()
         for file_name in dirs:
             self.decrypt_file(file_name)
+    
 
+obj = Encryption(bytes.fromhex(os.getenv('KEY'))) # Fetching Key from envrionment
+clear = lambda: os.system('cls') # Function for clearing cli screen
 
-key = b'[EX\xc8\xd5\xbfI{\xa2$\x05(\xd5\x18\xbf\xc0\x85)\x10nc\x94\x02)j\xdf\xcb\xc4\x94\x9d(\x9e'
-enc = Encryptor(key)
-clear = lambda: os.system('cls')
-
-if os.path.isfile('data.txt.enc'):
+if os.path.isfile('data.txt.enc'): # If secured password file is created
     while True:
-        password = str(input("Enter password: "))
-        enc.decrypt_file("data.txt.enc")
+        password = getpass.getpass("Enter Password :")
+        obj.decrypt_file("data.txt.enc")
         p = ''
-        with open("data.txt", "r") as f:
+        with open("data.txt",'r') as f:
             p = f.readlines()
         if p[0] == password:
-            enc.encrypt_file("data.txt")
+            obj.encrypt_file("data.txt")
             break
-
-    while True:
+    while True: # taking input
         clear()
         choice = int(input(
             "1. Press '1' to encrypt file.\n2. Press '2' to decrypt file.\n3. Press '3' to Encrypt all files in the directory.\n4. Press '4' to decrypt all files in the directory.\n5. Press '5' to exit.\n"))
         clear()
         if choice == 1:
-            enc.encrypt_file(str(input("Enter name of file to encrypt: ")))
+            obj.encrypt_file(input("Enter name of file to encrypt: "))
         elif choice == 2:
-            enc.decrypt_file(str(input("Enter name of file to decrypt: ")))
+            obj.decrypt_file(input("Enter name of file to decrypt: "))
         elif choice == 3:
-            enc.encrypt_all_files()
+            obj.encrypt_all_files()
         elif choice == 4:
-            enc.decrypt_all_files()
+            obj.decrypt_all_files()
         elif choice == 5:
             exit()
         else:
             print("Please select a valid option!")
-
-else:
+else: # If there is no data.txt.enc file
     while True:
         clear()
-        password = str(input("Setting up stuff. Enter a password that will be used for decryption: "))
-        repassword = str(input("Confirm password: "))
+        password = getpass.getpass("Setting up stuff. Enter a password that will be used for decryption: ") # Takes password
+        repassword = getpass.getpass("Confirm password: ") # Confirmation for previous input
         if password == repassword:
             break
         else:
             print("Passwords Mismatched!")
-    f = open("data.txt", "w+")
-    f.write(password)
-    f.close()
-    enc.encrypt_file("data.txt")
+            
+    with open("data.txt",'w+') as f:
+        f.write(password)
+
+    obj.encrypt_file("data.txt")
     print("Please restart the program to complete the setup")
     time.sleep(15)
